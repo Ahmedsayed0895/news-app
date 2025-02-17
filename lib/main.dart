899ae34556ad8315d2f01d35d84533e60ab728a9
Ahmed Sayed <12422022647994@pg.cu.edu.eg>
2025-02-17 04:10:19 +0200

    App init

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const News());
+}
+
+class News extends StatelessWidget {
+  const News({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
